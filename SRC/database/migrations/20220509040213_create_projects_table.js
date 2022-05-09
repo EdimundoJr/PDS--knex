ebc426947a192c07d50147c3fda0e982ca8c2ae9
Edimundo Junior <juniorconds@gmail.com>
2022-05-09 15:09:02 -0300

    Procedures & triggers

--- SRC/database/migrations/20220509040213_create_projects_table.js
+++ SRC/database/migrations/20220509040213_create_projects_table.js
@@ -1,17 +1,19 @@
-exports.up = knex => knex.schema.createTable('projects', table => {
-  table.increments('id')
-  table.text('title')
+const { onUpdateTrigger } = require('../../../knexfile')
+exports.up = async knex =>
+  knex.schema
+    .createTable('projects', table => {
+      table.increments('id')
+      table.text('title')
 
-  // relacionamento 
-  table.integer('user_id')
-       .references('users.id')
-       .notNullable()
-       .onDelete('CASCADE')
+      // relacionamento
+      table
+        .integer('user_id')
+        .references('users.id')
+        .notNullable()
+        .onDelete('CASCADE')
 
-  table.timestamps(true, true)
- 
+      table.timestamps(true, true)
+    })
+    .then(() => knex.raw(onUpdateTrigger('projects')))
 
-})
-
-exports.down = knex => knex.schema.dropTable('projects')
-
+exports.down = async knex => knex.schema.dropTable('projects')
